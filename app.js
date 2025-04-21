1265ae8ca09e3702e9696acc6bae0b3b617eab4b
Daud Khan <daudu6944@gmail.com>
2025-04-21 18:48:47 +0500

    setting up project

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const path = require('path')
 
 const server = express()
 
+server.set('view engine', 'ejs')
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
+server.use(express.static(path.join(__dirname, "public")))
 
 server.get('/', (req, res) => {
-    res.send('Connected...')
+    res.render('index')
 
+})
+
+server.get('/read', (req, res) => {
+    res.render('read')
 })
 
 
