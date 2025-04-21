2b7119a5d5a3eef5f7e011f895e6cc43c443c640
Daud Khan <daudu6944@gmail.com>
2025-04-21 15:56:17 +0500

    Initialize project for crud using express & mongodb

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const server = express()
+
+
+server.get('/', (req, res) => {
+    res.send('Connected...')
+
+})
+
+
+server.listen('3000', () => {
+    console.log('listening...');
+})
